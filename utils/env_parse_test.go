e25098bb37ca063d070956c53a56f8c1d12ff779
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2021-11-09 16:37:22 -0800

    Fix parsing of Mapping rename_only field. (#19)

--- utils/env_parse_test.go
+++ utils/env_parse_test.go
@@ -2,13 +2,16 @@ package utils
 
 import (
 	"encoding/json"
+	"strconv"
 	"testing"
 )
 
 type testStruct1 struct {
 	A1 string      `json:"a1"`
 	A2 testStruct2 `json:"b"`
+	A3 bool        `json:"a3"`
 }
+type tmpTestStruct1 testStruct1
 
 type testStruct2 struct {
 	B1 string        `json:"b1"`
@@ -19,6 +22,31 @@ type testStruct2 struct {
 type testStruct3 struct {
 	C1 string `json:"c1"`
 	C2 string `json:"c2"`
+}
+
+// Demonstrating the use of a custom Unmarshaler
+// to support string versions of bools.
+func (ts *testStruct1) UnmarshalJSON(dat []byte) error {
+	d := map[string]interface{}{}
+	if err := json.Unmarshal(dat, &d); err != nil {
+		return err
+	}
+	var err error
+	if a3, ok := d["a3"]; ok {
+		if d["a3"], err = strconv.ParseBool(a3.(string)); err != nil {
+			return err
+		}
+	}
+	t, err := json.Marshal(d)
+	if err != nil {
+		return err
+	}
+	tts := tmpTestStruct1{}
+	if err := json.Unmarshal(t, &tts); err != nil {
+		return err
+	}
+	*ts = testStruct1(tts)
+	return nil
 }
 
 func TestParseCLI(t *testing.T) {
@@ -38,6 +66,7 @@ func TestParseCLI(t *testing.T) {
 				},
 			},
 		},
+		A3: true,
 	}
 	testData := []string{
 		"a1=aaa",
@@ -47,6 +76,7 @@ func TestParseCLI(t *testing.T) {
 		"b.b3[0].c2=ttt",
 		"b.b3[1].c1=yyy",
 		"b.b3[1].c2=ooo",
+		"a3=true",
 	}
 	actualData := testStruct1{}
 
