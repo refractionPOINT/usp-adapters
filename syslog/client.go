f54ecd21739e08d40a1783f8e801812357bada6d
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2022-10-10 09:50:01 -0700

    Fix tests.

--- syslog/client.go
+++ syslog/client.go
@@ -2,12 +2,10 @@ package usp_syslog
 
 import (
 	"crypto/tls"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"net"
-	"strconv"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -50,45 +48,6 @@ func (c *SyslogConfig) Validate() error {
 	if c.Port == 0 {
 		return errors.New("missing port")
 	}
-	return nil
-}
-
-// This custom JSON Unmarshaler permits the `IsUDP` value to be
-// loaded either from a bool or a string (from an environment variable for example).
-type tempSyslogConfig SyslogConfig
-
-func (sc *SyslogConfig) UnmarshalJSON(data []byte) error {
-	// First get all the fields parsed in a dictionary.
-	d := map[string]interface{}{}
-	if err := json.Unmarshal(data, &d); err != nil {
-		return err
-	}
-
-	// Check if the `rename_only` field is present and if
-	// it is, is it a string?
-	var err error
-	if iu, ok := d["is_udp"]; ok {
-		if ius, ok := iu.(string); ok {
-			if d["is_udp"], err = strconv.ParseBool(ius); err != nil {
-				return err
-			}
-		}
-	}
-
-	// Re-marshal to JSON so that we can
-	// do another single-pass Unmarshal.
-	t, err := json.Marshal(d)
-	if err != nil {
-		return err
-	}
-
-	// Finally extract to a temporary type
-	// (to bypass this custom Unmarshaler).
-	tsc := tempSyslogConfig(*sc)
-	if err := json.Unmarshal(t, &tsc); err != nil {
-		return err
-	}
-	*sc = SyslogConfig(tsc)
 	return nil
 }
 
