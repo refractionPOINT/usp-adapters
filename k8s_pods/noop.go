8796c9943cebc7183adbd9cf48813b50f1322d7e
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2023-06-09 18:29:56 -0700

    Noop for aix.

---
+++ k8s_pods/noop.go
@@ -0,0 +1,19 @@
+//go:build aix
+// +build aix
+
+package usp_k8s_pods
+
+import "errors"
+
+// Dummy noop file to build when the platform
+// is _not_ supported.
+
+type K8sPodsAdapter struct{}
+
+func NewK8sPodsAdapter(conf FileConfig) (*K8sPodsAdapter, chan struct{}, error) {
+	return nil, nil, errors.New("k8s_pods collection not supported on this platform")
+}
+
+func (a *K8sPodsAdapter) Close() error {
+	return nil
+}
