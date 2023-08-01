0920ac0a75d217a71a9b4c9a4dc802bc0cecbed5
Joshua Padilla <joshua@refractionpoint.com>
2023-08-01 15:18:19 -0500

    attempting to fix build

--- bigquery/noop.go
+++ bigquery/noop.go
@@ -3,26 +3,14 @@
 
 package usp_bigquery
 
-import (
-	"errors"
-	"net"
-	"time"
-)
+import "errors"
 
-var zeroTime time.Time
+type BigQueryAdapter struct{}
 
-// socketConn is a wrapped net.Conn that tries to do connectivity check.
-type socketConn struct {
-	net.Conn
-
-	buffer [1]byte
-	closed atomic.Int32
+func NewBigQueryAdapter(conf BigQueryConfig) (*BigQueryAdapter, chan struct{}, error) {
+	return nil, nil, errors.New("BigQuery is not supported on AIX")
 }
 
-func (sc *socketConn) read0() error {
-	return errors.New("function read0 is not available on AIX")
-}
-
-func (sc *socketConn) checkConn() error {
-	return errors.New("function checkConn is not available on AIX")
+func (a *BigQueryAdapter) Close() error {
+	return nil
 }
