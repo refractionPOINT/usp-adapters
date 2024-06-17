d462c7a827caf2203c9743c01bca5d53f7b69284
Maxime Lamothe-Brassard <maxime@refractionpoint.com>
2024-06-17 10:02:20 -0600

    Deduping api (#164)
    * Adding a deduper interface and test for a simple local one.
    
    * Move o365 deduping to be interface-based.
    
    * Re-introduce the per Id deduping.
    
    * Fix ID

---
+++ utils/deduping_test.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"testing"
+	"time"
+)
+
+func TestLocalDeduper_CheckAndAdd(t *testing.T) {
+	window := 100 * time.Millisecond
+	ttl := 500 * time.Millisecond
+
+	d, err := NewLocalDeduper(window, ttl)
+	if err != nil {
+		t.Fatalf("error creating deduper: %v", err)
+	}
+
+	if d.CheckAndAdd("key1") {
+		t.Error("key1 should not exist")
+	}
+	if !d.CheckAndAdd("key1") {
+		t.Error("key1 should exist")
+	}
+	if d.CheckAndAdd("key2") {
+		t.Error("key2 should not exist")
+	}
+	if !d.CheckAndAdd("key2") {
+		t.Error("key1 should exist")
+	}
+	time.Sleep(ttl + time.Second)
+	if d.CheckAndAdd("key1") {
+		t.Error("key1 should not exist after ttl")
+	}
+	if d.CheckAndAdd("key2") {
+		t.Error("key1 should exist after ttl")
+	}
+	if d.CheckAndAdd("key3") {
+		t.Error("key3 should not exist")
+	}
+
+	d.Close()
+}
